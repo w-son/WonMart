e07fbdf20ca0014fdd7e20ed92ec985f164c7e9d
w-son <williamson@naver.com>
2020-02-07 02:15:05 +0900

    LetterForm validation added, NavBar edited

--- src/main/java/WonMart/WonMart/controller/LetterController.java
+++ src/main/java/WonMart/WonMart/controller/LetterController.java
@@ -7,11 +7,13 @@ import WonMart.WonMart.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -28,13 +30,24 @@ public class LetterController { // 쪽지 생성, 쪽지 조회
 
         model.addAttribute("receiver_id", id);
         model.addAttribute("receiver_nickName", receiver_nickName);
-        model.addAttribute("form", new LetterForm());
+        model.addAttribute("letterForm", new LetterForm());
 
         return "letter/createLetterForm";
     }
 
     @PostMapping("/letter/{receiver_id}/new")
-    public String send(@PathVariable("receiver_id") Long receiver_id, HttpSession session, LetterForm form) {
+    public String send(
+            @PathVariable("receiver_id") Long receiver_id,
+            @Valid LetterForm form, BindingResult result,
+            Model model, HttpSession session) {
+
+        if(result.hasErrors()) {
+            Member receiver = memberService.findOne(receiver_id);
+            String receiver_nickName = receiver.getNickName();
+            model.addAttribute("receiver_nickName", receiver_nickName);
+            return "letter/createLetterForm";
+        }
+
         Long sender_id = (Long) session.getAttribute("member_id");
         letterService.send(sender_id, receiver_id, form.getBody());
 
--- src/main/java/WonMart/WonMart/controller/LetterForm.java
+++ src/main/java/WonMart/WonMart/controller/LetterForm.java
@@ -3,9 +3,12 @@ package WonMart.WonMart.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class LetterForm {
 
+    @NotEmpty(message = "내용을 입력해주세요")
     private String body;
 
 }
