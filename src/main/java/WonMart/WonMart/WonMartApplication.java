07e396d0e29f696036734e854211918efb3fda4a
w-son <williamson@naver.com>
2020-03-23 22:58:06 +0900

    gitignore 추가

--- src/main/java/WonMart/WonMart/WonMartApplication.java
+++ src/main/java/WonMart/WonMart/WonMartApplication.java
@@ -13,6 +13,7 @@ public class WonMartApplication {
 	 - apache commons 추가
 	 - jackson core 추가
 	 - querydsl 추가 (4, 44, 51-68)
+	 - .gitignore 관련 https://emflant.tistory.com/127
 
 	 주석 작성 요령
 	 - 엔티티끼리 중복되는 함수에 대한 설명 주석은 모든 엔티티에 작성하지 않고 Member관련 클래스에 작성
